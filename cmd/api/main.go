04982efec9ecc8788dc6a47cb2e4a9592b9b7e61
Drew Churchill <dmc9870@gmail.com>
2022-03-09 16:05:55 -0600

    Adding Postgres connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/andrewchurchill/go-tutorial/models"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -20,11 +25,15 @@ type AppStatus struct {
 type configuration struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
 	config configuration
 	logger *log.Logger
+	models models.Models
 }
 
 func main() {
@@ -32,13 +41,25 @@ func main() {
 	var cfg configuration
 	flag.IntVar(&cfg.port, "port", 4000, "The server will listen to this port.")
 	flag.StringVar(&cfg.env, "env", "development", "The application environment (development|production).")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:password@localhost/go_movies?sslmode=disable", "Postgres connection string")
 	flag.Parse()
 
 	// Set up logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Set up database
+	db, err := openDb(cfg)
+	if err != nil {
+		logger.Fatal(err)
+		return
+	}
+
+	defer db.Close()
+
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: models.NewModels(db),
 	}
 
 	srv := &http.Server{
@@ -51,8 +72,25 @@ func main() {
 
 	fmt.Printf("Starting server on port %d...\n", cfg.port)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		app.logger.Println(err)
 	}
+}
+
+func openDb(cfg configuration) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- cmd/api/movieHandlers.go
+++ cmd/api/movieHandlers.go
@@ -4,9 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
-	"time"
 
-	"github.com/andrewchurchill/go-tutorial/models"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -22,17 +20,11 @@ func (app *application) getMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.logger.Println(id)
 
-	movie := models.Movie{
-		Id:          id,
-		Title:       "Some movie",
-		Description: "Some description",
-		Year:        2021,
-		ReleaseDate: time.Date(2021, 1, 1, 1, 0, 0, 0, time.Local),
-		Runtime:     100,
-		Rating:      5,
-		MpaaRating:  "PG-13",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+	movie, err := app.models.DB.Get(id)
+	if err != nil {
+		app.logger.Println(err)
+		app.errorJson(w, err)
+		return
 	}
 
 	err = app.writeJson(w, http.StatusOK, movie, "data")
